f762da45bdf7d2d2132946115b36df38f1837422
Lukasz Szawronski <lukasz.szawronski@gmail.com>
2022-05-19 11:50:42 +0200

    My new Project

---
+++ src/main/java/pl/lukaszszawronski/inventoryapp/InventoryAppApplication.java
@@ -0,0 +1,13 @@
+package pl.lukaszszawronski.inventoryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/lukaszszawronski/inventoryapp/IventoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lukaszszawronski.inventoryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IventoryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
